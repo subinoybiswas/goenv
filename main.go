3f69a595bef8548f97953370a4e23c8d97de2a06
Subinoy Biswas <heysubinoy@gmail.com>
2024-02-15 22:31:28 +0530

    package

--- main.go
+++ main.go
@@ -2,43 +2,23 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
-type a struct {
-	Hi   string
-	Meow string
-}
-
-func getEnv(key string) (string, error) {
+func GetEnv(key string) (string, error) {
 	if err := godotenv.Load(".env"); err != nil {
-		return envGetter(key)
+		return EnvGetter(key)
 	}
-	return getEnvFrmFile(key)
+	return GetEnvFrmFile(key)
 }
-func getEnvFrmFile(key string) (string, error) {
-	return envGetter(key)
+func GetEnvFrmFile(key string) (string, error) {
+	return EnvGetter(key)
 }
-func envGetter(key string) (string, error) {
+func EnvGetter(key string) (string, error) {
 	if value, exists := os.LookupEnv(key); exists {
 		return value, nil
 	}
 	return "", errors.New("ENV ERROR")
 }
-
-
-func main() {
-	var envName string
-	fmt.Scanln(&envName)
-	value, err := getEnv(envName)
-	if err!=nil{
-		fmt.Println(err)
-		os.Exit(0)
-	}
-	fmt.Println(value)
-
-}
-
